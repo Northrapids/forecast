716771c6a817dec7344e009a461e9f949e718506
Northrapids <nordfors.fredrik@gmail.com>
2023-09-22 11:42:25 +0200

    added missing @Repository

--- forecast/src/main/java/com/demo/forecast/repositories/ForecastRepository.java
+++ forecast/src/main/java/com/demo/forecast/repositories/ForecastRepository.java
@@ -5,12 +5,15 @@ import com.demo.forecast.models.Forecast;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+
+@Repository
 public interface ForecastRepository extends CrudRepository<Forecast, UUID> {
     @Override
     Optional<Forecast> findById(UUID id);
